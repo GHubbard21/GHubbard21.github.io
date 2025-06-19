c94b3b884dd6ffae742d27f312f4a6585d2728b5
Grady Hubbard <g21@tomhub.com>
2025-06-19 14:40:28 -0400

    Clock fix, Window open/close. Console window.

--- Scripts/homescript.js
+++ Scripts/homescript.js
@@ -6,13 +6,35 @@ const clock = document.getElementById("clock");
 
 function clockUpdate(){
     let time = new Date();
-    clock.innerHTML = time.toLocaleTimeString('en-US', { 
+
+    let day = time.toLocaleDateString('en-US', { weekday: 'long' });
+
+    let currentTime = time.toLocaleTimeString('en-US', { 
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit',
         hour12: true
     });
 
+    clock.innerHTML = day + " " + currentTime;
+
 }
 setInterval(clockUpdate, 1000);
 clockUpdate();
+
+
+function openWindow(id){
+    document.getElementById(id).style.display = "block";
+}
+
+function closeWindow(id){
+    document.getElementById(id).style.display = "none";
+}
+
+function openConsole(id){
+    document.getElementById(id).style.display = "block";
+}
+
+function closeConsole(id){
+    document.getElementById(id).style.display = "none";
+}
